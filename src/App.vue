461736609cf8855fc3d7b67f8b52a547f2f80d63
Sota <sota.sumii@gmail.com>
2023-05-22 23:37:48 +0900

    add skill

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <header>
+    <img src="./assets/logo.png" alt="">
     <ul>
       <li><a href="#top">top</a></li>
       <li><a href="#about">about me</a></li>
       <li><a href="#works">works</a></li>
+      <li><a href="#skill">skill</a></li>
     </ul>
   </header>
   <main>
+    <div class="head">
+      
+    </div>
     <div class="container">
       <div class="top mb-50" id="top">
-        <p><span>好き</span>なことだけで、<br><span>生き</span>ていく</p>
+        <p>好きなことだけで、<br>生きていく</p>
         <img src="./assets/myphoto-beach.jpg" alt="image">
       </div>
       <div class="about_me mb-50" id="about">
@@ -23,7 +28,7 @@
           </ul>
           <ul class="right-card">
             <li>住井奏太</li>
-            <li>早稲田大学創造理工学部経営システム工学科2年</li>
+            <li>早稲田大学創造理工学部経営システム工学科 2年</li>
             <li>2004年1月10日</li>
             <li>旅、勉強、寝ること</li>
           </ul>
@@ -35,17 +40,28 @@
           <WorksComp :img="content.img" :url="content.url" :title="content.title" class="work"/>
         </template>
       </div>
+      <div class="skill mb-50" id="skill">
+        <h1 class="title">Skill set</h1>
+        <template v-for="graph in graphs" :key="graph">
+          <Graph :skill="graph.skill" :number="graph.number" class="graph"/>
+        </template>
+      </div>
     </div>
   </main>
+  <footer>
+    <h1>Thank You !!</h1>
+  </footer>
 </template>
 
 <script>
 import WorksComp from './components/Works.vue'
+import Graph from './components/Graphs.vue'
 
 export default {
   name: 'App',
   components: {
-    WorksComp
+    WorksComp,
+    Graph
   },
   data(){
     return{
@@ -58,6 +74,54 @@ export default {
           title:"タイピングゲーム",
           url:"https://sota0110.github.io/typingGame-vue/",
           img:require("./assets/typing-game.png")
+        }
+      ],
+      graphs:[
+        {
+          skill:"Python",
+          number:5
+        },{
+          skill:"HTML/CSS",
+          number:4
+        },{
+          skill:"Javascript",
+          number:3
+        },{
+          skill:"Vue.js",
+          number:5
+        },{
+          skill:"PHP",
+          number:4
+        },{
+          skill:"CakePHP",
+          number:5
+        },{
+          skill:"MySQL",
+          number:4
+        },{
+          skill:"PostgreSQL",
+          number:3
+        },{
+          skill:"R",
+          number:1
+        },{
+          skill:"C",
+          number:3
+        },{
+          skill:"C#",
+          number:1
+        },{
+          skill:"Unity",
+          number:1
+        },{
+          skill:"Git",
+          number:5
+        },{
+          skill:"Linux",
+          number:3
+        },{
+          skill:"Docker",
+          number:2
         }
       ]
     }
---
+++ src/components/Graphs.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+      <p>{{ skill }}</p>
+      <div class="skillGraph-wrapper">
+        <div :style="gauge" class="skillGraph"></div>
+      </div>
+      <div class="border">
+        
+      </div>
+    </div>
+</template>
+  
+<script>
+  export default {
+    name: 'performance-graph',
+    props:{
+      skill: String,
+      number: String
+    },
+    computed:{
+      gauge:function(){
+        return{
+          'width':20*this.number+"%"
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+@import 'graphs-style.css';
+</style>
