e849c0c2d5c78062455e5bfdf9918d0ce7ca256a
Sota <sota.sumii@gmail.com>
2023-05-27 00:45:37 +0900

    responsive

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <a href="http://sota.starfree.jp/portfolio/">
       <img src="./assets/logo.png" alt="">
     </a>
-    <ul>
+    <ul class="header-1000">
       <li><a href="#top">top</a></li>
       <li><a href="#about">about me</a></li>
       <li><a href="#works">works</a></li>
@@ -11,6 +11,22 @@
       <li><a href="#skill">skill</a></li>
       <li><a href="#contact">contact me</a></li>
     </ul>
+    <nav class="sp-nav" v-if="menu">
+      <ul>
+        <li class="close"><a @click="closeMenu"></a></li>
+        <li><a href="#top" class="menu" @click="closeMenu">top</a></li>
+        <li><a href="#about" class="menu" @click="closeMenu">about me</a></li>
+        <li><a href="#works" class="menu" @click="closeMenu">works</a></li>
+        <li><a href="#career" class="menu" @click="closeMenu">career</a></li>
+        <li><a href="#skill" class="menu" @click="closeMenu">skill</a></li>
+        <li><a href="#contact" class="menu" @click="closeMenu">contact me</a></li>
+      </ul>
+    </nav>
+    <div class="hamburger" @click="showMenu">
+      <div class="hamburger-line"></div>
+      <div class="hamburger-line"></div>
+      <div class="hamburger-line"></div>
+    </div>
   </header>
   <main>
     <div class="head">
@@ -27,12 +43,13 @@
           <ul class="left-card">
             <li>名前</li>
             <li>所属</li>
+            <li class="br-1000">&nbsp;</li>
             <li>生年月日</li>
             <li>趣味</li>
           </ul>
           <ul class="right-card">
-            <li>住井奏太</li>
-            <li>早稲田大学創造理工学部経営システム工学科 2年</li>
+            <li>住井奏太(すみいそうた)</li>
+            <li>早稲田大学創造理工学部<br class="br-1000">経営システム工学科 2年</li>
             <li>2004年1月10日</li>
             <li>旅、勉強、寝ること</li>
           </ul>
@@ -40,7 +57,7 @@
       </div>
       <div class="works back pb-50" id="works">
         <h1 class="title">Works</h1>
-        <p class="description">僕が作成したWebアプリや僕が出演した記事などをご紹介します！</p>
+        <p class="description">僕が作成したWebアプリや<br class="br-670">僕が出演した記事などをご紹介します！</p>
         <template v-for="content in contents" :key="content">
           <WorksComp :img="content.img" :url="content.url" :title="content.title" class="work"/>
         </template>
@@ -50,27 +67,29 @@
         <p class="description">僕のインターン歴です！</p>
         <div class="card h-career">
           <ul class="left-card">
-            <li>22年1月～22年9月</li>
-            <li>23年2月～22年2月</li>
+            <li>22年1月～9月</li>
+            <li class="br-1000">&nbsp;</li>
+            <li>23年2月～2月</li>
+            <li class="br-670">&nbsp;</li>
             <li>23年2月～現在</li>
           </ul>
           <ul class="right-card">
-            <li>グローバルパートナーズ株式会社 インターン</li>
-            <li>株式会社パブコ 短期インターン</li>
-            <li>株式会社ハートビーツ インフラエンジニア</li>
+            <li>グローバルパートナーズ<br class="br-1000">株式会社 インターン</li>
+            <li>株式会社パブコ <br class="br-670">短期インターン</li>
+            <li>株式会社ハートビーツ <br class="br-1000">インフラエンジニア</li>
           </ul>
         </div>
       </div>
       <div class="skill back pb-50" id="skill">
         <h1 class="title">Skill set</h1>
-        <p class="description">僕が今まで勉強してきた技術たちです！スキルレベルをグラフで表しています！</p>
+        <p class="description">僕が今まで勉強してきた技術たちです！<br class="br-1000">スキルレベルをグラフで表しています！</p>
         <template v-for="graph in graphs" :key="graph">
           <Graph :skill="graph.skill" :number="graph.number" class="graph"/>
         </template>
       </div>
       <div class="contact pb-50" id="contact">
         <h1 class="title">Contact me</h1>
-        <p class="description">最後までご覧いただきありがとうございます！<br>もしコメントがありましたら下記フォームよりお問い合わせください。</p>
+        <p class="description">最後までご覧いただきありがとうございます！<br>もしコメントがありましたら<br class="br-670">下記フォームよりお問い合わせください。</p>
         <transition>
           <p v-show="alert" class="sent_mail">メールを送信しました</p>
         </transition>
@@ -80,18 +99,18 @@
           <p>Email</p>
           <input type="email" v-model="email">
           <p>Comment</p>
-          <textarea cols="60" rows="6" v-model="comment"></textarea><br>
+          <textarea rows="6" v-model="comment" class="textarea"></textarea><br>
           <input type="submit" name="send" value="SEND" class="submit-btn" @click="sendMail">
           <input type="text" name="name" v-model="sendName" class="erase">
           <input type="email" name="email" v-model="sendEmail" class="erase">
-          <textarea name="comment" cols="60" rows="6" v-model="sendComment" class="erase"></textarea>
+          <textarea name="comment" v-model="sendComment" class="erase"></textarea>
         </form>
         <iframe name="form" scrolling="no" frameborder="no" class="erase"></iframe>
       </div>
     </div>
   </main>
   <footer>
-    <h1>Thank You !!</h1>
+    <h1>Thank <br class="br-670">You !!</h1>
   </footer>
 </template>
 
@@ -120,6 +139,12 @@ export default {
       this.alert = true;
       document.form.submit();
       return false;
+    },
+    showMenu(){
+      this.menu = true;
+    },
+    closeMenu(){
+      this.menu = false;
     }
   },
   watch: {
@@ -138,31 +163,32 @@ export default {
       sendEmail:"",
       sendComment:"",
       alert:false,
+      menu:false,
       contents:[
         {
           title:"ちずアプリ",
           url:"http://sota.starfree.jp/gmap-api/",
           img:require("./assets/gmap-app.png")
         },{
-          title:"タイピングゲーム",
+          title:"Typing Game",
           url:"https://sota0110.github.io/typingGame-vue/",
           img:require("./assets/typing-game.png")
         },{
-          title:"BEAULABOディレクター紹介",
+          title:"BEAULABO",
           url:"https://note.com/insidebeau/n/ne503efaf52ca?magazine_key=m57f040bca237",
           img:require("./assets/beau.png")
         },{
-          title:"Teenz Discuss(YouTube)",
+          title:"CLUB CEO",
+          url:"https://audee.jp/voice/show/52854",
+          img:require("./assets/clubceo.jpg")
+        },{
+          title:"Teenz Discuss (YouTube)",
           url:"https://www.youtube.com/watch?v=roaMO1Sx1wk&t=1s",
           img:require("./assets/discuss.png")
         },{
-          title:"気になる10代名鑑",
+          title:"Steenz  気になる10代名鑑",
           url:"https://steenz.jp/n/nea1cc5738b86",
           img:require("./assets/steenz.png")
-        },{
-          title:"CLUB CEO(ラジオ)",
-          url:"https://audee.jp/voice/show/52854",
-          img:require("./assets/clubceo.jpg")
         }
       ],
       graphs:[
@@ -220,4 +246,5 @@ export default {
 
 <style scoped>
 @import 'style.css';
+@import 'responsive-style.css';
 </style>
